4fcff33244a38560458faf7f55a23ece11749c68
MANDONGI <ddongmoney@hotmail.com>
2015-07-01 14:55:25 +0900

    Uploaded Queue and Stack library

---
+++ Data-Types/Queue-and-Stack/Queue n Stack.js
@@ -0,0 +1,81 @@
+function Queue(){
+	if(this==(function(){return this;}).call(null))return Queue.apply({},Array.prototype.slice.call(arguments));
+	for(i=0;i<arguments.length;i++)this[i]=arguments[i];
+	this.length=arguments.length;
+	this.iterateOver=function(callback){
+		for(i=0;i<this.length;i++)callback(this[i],i,this);
+	};
+	this.put=function(){
+		for(v=0;v<arguments.length;v++)this[this.length++]=arguments[v];
+		return Array.prototype.slice.call(arguments);
+	};
+	this.get=function(v){
+		var tmp=[];
+		for(i=0;i<this.length;i++){
+			if(i<v){
+				tmp.push(this[i]);
+				delete this[i];
+			}
+			else{
+				this[i-v]=this[i];
+				delete this[i];
+			}
+		}
+		this.length=this.length-v;
+		return tmp;
+	};
+	this.toArray=function(){
+		var tmp=[];
+		this.iterateOver(function(v){tmp.push(v);});
+		return tmp;
+	};
+	this.toStack=function(){
+		return Stack.apply({},this.toArray());
+	};
+	this.toString=function(){
+		return this.toArray().toString();
+	};
+	return this;
+}
+Queue.fromArray=function(arr){
+	return Queue.apply({},arr);
+};
+Queue.fromStack=function(stack){
+	return Queue.apply({},stack.toArray());
+};
+function Stack(){
+	if(this==(function(){return this;}).call(null))return Stack.apply({},Array.prototype.slice.call(arguments));
+	for(i=0;i<arguments.length;i++)this[i]=arguments[i];
+	this.length=arguments.length;
+	this.iterateOver=function(callback){
+		for(i=0;i<this.length;i++)callback(this[i],i,this);
+	};
+	this.push=function(){
+		for(v=0;v<arguments.length;v++)this[this.length++]=arguments[v];
+		return Array.prototype.slice.call(arguments);
+	};
+	this.pop=function(v){
+		var tmp=[];
+		for(i=this.length-v;i<this.length;i++){tmp.push(this[i]);delete this[i];}
+		this.length=this.length-v;
+		return tmp;
+	};
+	this.toArray=function(){
+		var tmp=[];
+		this.iterateOver(function(v){tmp.push(v);});
+		return tmp;
+	};
+	this.toQueue=function(){
+		return Queue.apply({},this.toArray());
+	};
+	this.toString=function(){
+		return this.toArray().toString();
+	};
+	return this;
+}
+Stack.fromArray=function(arr){
+	return Stack.apply({},arr);
+};
+Stack.fromQueue=function(queue){
+	return Stack.apply({},queue.toArray());
+};
